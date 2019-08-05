ef47ce3f4b50b304c6b007cf8bb1d3d350f8a3de
thig0w <thiago.we@gmail.com>
2019-08-05 11:47:41 -0300

    "Ponto estavel 1"

--- pyqt/Sefaz.py
+++ pyqt/Sefaz.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+import io
 import logging
 import re
 
+
 from PIL import Image
 from PyQt5.QtCore import QThread
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,43 +15,60 @@ logger = logging.getLogger(__name__)
 
 
 class Sefaz(QThread):
-    def __init__(self, url):
+    def __init__(self, url, driver):
         super().__init__()
         key = re.match(".*=([0-9]{44}\|[0-9]\|[0-9]\|[0-9]\|[A-Z0-9]{40})", url.upper())
-        print(key.groups())
-        logger.debug('URL: %s', key.groups()[0])
-        self.url = 'https://www.sefaz.rs.gov.br/ASP/AAE_ROOT/NFE/SAT-WEB-NFE-NFC_QRCODE_1.asp?p=' + \
-                   key.groups()[0]
+        logger.debug("URL: %s", key.groups()[0])
+        self.url = (
+            "https://www.sefaz.rs.gov.br/ASP/AAE_ROOT/NFE/SAT-WEB-NFE-NFC_QRCODE_1.asp?p="
+            + key.groups()[0]
+        )
         self.total_value = 0
+        self.date = ""
+        self.driver = driver
 
     def run(self):
-        driver = webdriver.PhantomJS("./phantomjs.exe")  # the normal SE phantomjs binding
-        driver.set_window_size(1024, 768)
-        driver.get(self.url)  # whatever reachable url
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="respostaWS"]')))
-        self.total_value = driver.find_element_by_xpath(
-            '//*[@id="respostaWS"]/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr/td/table/tbody/tr[6]/td/table/tbody/tr[1]/td[2]').text
-        element = driver.find_element_by_xpath('//*[@id="respostaWS"]')
+        # driver = webdriver.PhantomJS(executable_path=executable_path)  # the normal SE phantomjs binding
+        self.driver.set_window_size(1024, 768)
+        self.driver.get(self.url)  # whatever reachable url
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="respostaWS"]'))
+        )
+        self.total_value = self.driver.find_element_by_xpath(
+            '//*[@id="respostaWS"]/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr/td/table/tbody/tr[6]/td/table/tbody/tr[1]/td[2]'
+        ).text
+        nf_header = self.driver.find_element_by_xpath(
+            '//*[@id="respostaWS"]/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr[1]/td'
+        ).text
+        self.date = re.match(".*([0-9]{2}/[0-9]{2}/[0-9]{4}).*", nf_header).groups()[0]
+        element = self.driver.find_element_by_xpath('//*[@id="respostaWS"]')
         location = element.location
         size = element.size
-        x = location['x']
-        y = location['y']
-        width = location['x'] + size['width']
-        height = location['y'] + size['height']
+        x = location["x"]
+        y = location["y"]
+        width = location["x"] + size["width"]
+        height = location["y"] + size["height"]
 
-        driver.save_screenshot("C:\\Users\\LOGIC\\Desktop\\teste.png")  # screen.png is a big red rectangle :)
+        # self.driver.save_screenshot("C:\\Users\\LOGIC\\Desktop\\teste.png")  # screen.png is a big red rectangle :)
+        screen = self.driver.get_screenshot_as_png()
 
-        im = Image.open('C:\\Users\\LOGIC\\Desktop\\teste.png')
+        # im = Image.open('C:\\Users\\LOGIC\\Desktop\\teste.png')
+        im = Image.open(io.BytesIO(screen))
         im = im.crop((int(x), int(y), int(width), int(height)))
-        im.save('C:\\Users\\LOGIC\\Desktop\\teste_croped.png')
+        im.save("C:\\Users\\LOGIC\\Desktop\\teste_croped.png")
 
-        driver.quit()
+        self.driver.quit()
 
-        logger.debug('NFCe total value: %s', self.total_value)
+        logger.debug(
+            "NFCe total value: %s | Date: %s | url: %s",
+            self.total_value,
+            self.date,
+            self.url,
+        )
 
 
 if __name__ == "__main__" or __name__ == "__builtin__":
-    url = 'https://www.sefaz.rs.gov.br/NFCE/NFCE-COM.aspx?p=43190693015006003210651270004470561759268442|2|1|1|4242DC7DF093B2756A42B54D5E8AAA094070A7F8'
-    url_frame = 'https://www.sefaz.rs.gov.br/ASP/AAE_ROOT/NFE/SAT-WEB-NFE-NFC_QRCODE_1.asp?p=43190693015006003210651270004470561759268442|2|1|1|4242DC7DF093B2756A42B54D5E8AAA094070A7F8'
+    url = "https://www.sefaz.rs.gov.br/NFCE/NFCE-COM.aspx?p=43190693015006003210651270004470561759268442|2|1|1|4242DC7DF093B2756A42B54D5E8AAA094070A7F8"
+    url_frame = "https://www.sefaz.rs.gov.br/ASP/AAE_ROOT/NFE/SAT-WEB-NFE-NFC_QRCODE_1.asp?p=43190693015006003210651270004470561759268442|2|1|1|4242DC7DF093B2756A42B54D5E8AAA094070A7F8"
     a = Sefaz(url_frame)
     a.run()
--- pyqt/qrcode.py
+++ pyqt/qrcode.py
@@ -6,28 +6,42 @@
 #
 # WARNING! All changes made in this file will be lost!
 import logging
-from PyQt5 import QtCore, QtGui, QtWidgets
+
 from CamReader import CamReader
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QPixmap
 from Sefaz import Sefaz
-from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtCore import QObject, pyqtSlot
+from phantomjs_bin import executable_path
+from selenium import webdriver
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
 class Ui_Dialog(object):
+    def __init__(self):
+        MAX_THREADS = 1
+        self.nfes = {}
+        self.web_drivers = []
+
+        for i in range(MAX_THREADS):
+            self.web_drivers.append(
+                webdriver.PhantomJS(executable_path=executable_path)
+            )
+
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(632, 262)
         self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
         self.buttonBox.setGeometry(QtCore.QRect(280, 220, 341, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setStandardButtons(
+            QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok
+        )
         self.buttonBox.setObjectName("buttonBox")
         self.tableWidget = QtWidgets.QTableWidget(Dialog)
         self.tableWidget.setGeometry(QtCore.QRect(260, 10, 361, 201))
-        self.tableWidget.setRowCount(1)
+        # self.tableWidget.setRowCount(1)
         self.tableWidget.setColumnCount(3)
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.horizontalHeader().setVisible(True)
@@ -46,26 +60,36 @@ class Ui_Dialog(object):
         self.buttonBox.rejected.connect(Dialog.reject)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-        self.nfes = []
+    def populate_table(self):
+        col = 0
+        self.tableWidget.setRowCount(len(self.nfes))
+        for nf in self.nfes.__iter__():
+            item_url = QtWidgets.QTableWidgetItem(self.nfes[nf].url)
+            item_url.setFlags(QtCore.Qt.ItemIsEnabled)
+            item_date = QtWidgets.QTableWidgetItem(self.nfes[nf].date)
+            item_date.setFlags(QtCore.Qt.ItemIsEnabled)
+            item_total_value = QtWidgets.QTableWidgetItem(self.nfes[nf].total_value)
+            item_total_value.setFlags(QtCore.Qt.ItemIsEnabled)
+            self.tableWidget.setItem(col, 0, item_url)
+            self.tableWidget.setItem(col, 1, item_date)
+            self.tableWidget.setItem(col, 2, item_total_value)
+            col = col + 1
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.label.setText(_translate("Dialog", "Loading Camera"))
+        self.label.setText(_translate("Dialog", "Loading Camera..."))
 
     # @pyqtSlot(QImage)
     def setImage(self, image):
         self.label.setPixmap(QPixmap.fromImage(image))
+        self.populate_table()
 
     # @pyqtSlot('QString')
     def set_url(self, url):
-        if not(self.nfes.__contains__(url)):
-            self.nfes.append(url)
-            st = Sefaz(url)
-            st.start()
-            print(st.total_value)
-
-
+        if not (self.nfes.__contains__(url)):
+            self.nfes[url] = Sefaz(url, self.web_drivers.pop())
+            self.nfes[url].start()
 
 
 if __name__ == "__main__" or __name__ == "__builtin__":
