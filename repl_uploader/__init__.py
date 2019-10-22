65921b06c157ae02f3488bb752d8e1e6b4062209
thig0w <thiago.we@gmail.com>
2019-10-22 00:07:37 -0300

    trying to find error on Travis CD

--- repl_uploader/__init__.py
+++ repl_uploader/__init__.py
@@ -4,7 +4,7 @@
 import logging
 from .replicon import Replicon
 
-VERSION = "2.0.3"
+VERSION = "2.0.5"
 __AUTHOR__ = "Thiago Weidman (tw@weidman.com.br)"
 __COPYRIGHT__ = "(C) 2014-2019 Thiago Weidman. GNU GPL 3 or later."
 
